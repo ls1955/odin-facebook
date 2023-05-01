2f2821fec7e6bc3b9449b5643b7fff6c3e0e73c8
Cheong <xxlsxx1955@gmail.com>
2023-05-01 08:05:28 +0800

    Reestablish 1:n association between User and FriendRequest

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
-  has_many :sent_friend_requests, foreign_key: :sender_id, class_name: "FriendRequest"
-  has_many :received_friend_requests, foreign_key: :receiver_id, class_name: "FriendRequest"
-  has_many :pending_friend_requests, -> { where status: "pending" }, class_name: "FriendRequest", foreign_key: :receiver_id
-  has_many :accepted_friend_requests, -> { where status: "accepted" }, class_name: "FriendRequest", foreign_key: :receiver_id
-  has_many :rejected_friend_requests, -> { where status: "rejected" }, class_name: "FriendRequest", foreign_key: :receiver_id
+  has_many :friend_requests, foreign_key: "receiver_id"
+  # has_many :sent_friend_requests, foreign_key: :sender_id, class_name: "FriendRequest"
+  # has_many :received_friend_requests, foreign_key: :receiver_id, class_name: "FriendRequest"
+  # has_many :pending_friend_requests, -> { where status: "pending" }, class_name: "FriendRequest", foreign_key: :receiver_id
+  # has_many :accepted_friend_requests, -> { where status: "accepted" }, class_name: "FriendRequest", foreign_key: :receiver_id
+  # has_many :rejected_friend_requests, -> { where status: "rejected" }, class_name: "FriendRequest", foreign_key: :receiver_id
+  # has_many :friends, class_name: "User", foreign_key: "user_id"
+  # belongs_to :user, optional: true
 end
