4939b07de78e31c37fc519ca388724ee9fa656e0
Cheong <xxlsxx1955@gmail.com>
2023-05-02 20:11:32 +0800

    Implement FriendRequest::pending

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -3,4 +3,10 @@ class FriendRequest < ApplicationRecord
   belongs_to :receiver, class_name: "User"
 
   validates :sender_id, uniqueness: { scope: :receiver_id }
+
+  class << self
+    def pending(user)
+      where(sender_id: user.id).or(where(receiver_id: user.id))
+    end
+  end
 end
