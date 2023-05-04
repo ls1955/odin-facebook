d25f3868143524b8e75275a3ff819c073c89dcb2
Cheong <xxlsxx1955@gmail.com>
2023-05-04 21:23:21 +0800

    Implement #relevant(user)
    * A method which return all the relevant posts
      from the user.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :comments
+
+  scope :relevant, ->(user) { where(id: [user.id] + user.all_friends.pluck(:id)) }
 end
