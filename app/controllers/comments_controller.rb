dc6b3ab57030d770149910974d33e4b210739ea2
Cheong <xxlsxx1955@gmail.com>
2023-05-04 15:30:56 +0800

    Modify style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -58,13 +58,14 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def comment_params
-      params.require(:comment).permit(:body, :author_id, :post_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def comment_params
+    params.require(:comment).permit(:body, :author_id, :post_id)
+  end
 end
