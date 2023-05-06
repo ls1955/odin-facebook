808458831536734934cd25eb5e97c163e1fbf36b
Cheong <xxlsxx1955@gmail.com>
2023-05-06 08:44:24 +0800

    Include #profile_link helper

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,9 @@
 module ProfilesHelper
+  def profile_link(user)
+    if user.profile.present?
+      link_to "Profile", profile_path(user.profile)
+    else
+      link_to "Create Profile", new_profile_path
+    end
+  end
 end
