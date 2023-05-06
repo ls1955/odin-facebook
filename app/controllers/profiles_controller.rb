595a0dce7091b6f69aa1640eb6e91d08b00d1ee2
Cheong <xxlsxx1955@gmail.com>
2023-05-06 08:47:47 +0800

    Implement #new and #create

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,21 @@
 class ProfilesController < ApplicationController
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = current_user.build_profile profile_params
+
+    if @profile.save
+      redirect_to profile_path(@profile), notice: "Profile has been succesfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:profile_image, :biography)
+  end
 end
