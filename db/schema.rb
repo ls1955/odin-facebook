e7fbb87fbf2ec64f1c39cf2c8156ab7eb99a053f
Cheong <xxlsxx1955@gmail.com>
2023-05-06 08:21:04 +0800

    Add user references to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_132727) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_06_002016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_132727) do
     t.text "biography"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -108,4 +110,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_132727) do
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "profiles", "users"
 end
