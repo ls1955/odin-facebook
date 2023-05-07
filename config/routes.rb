d95a49b3c749213cf693f986a1daecf89fa5e621
Cheong <xxlsxx1955@gmail.com>
2023-05-07 10:50:39 +0800

    Include UsersController#show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  resources :users, only: :index
+  resources :users, only: %i[index show]
   resources :strangers, only: :index
   resources :friend_requests, only: %i[index create destroy] do
     member do
