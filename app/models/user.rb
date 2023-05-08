072086722f2ac1cc8436817bbb5732270a1c8403
Cheong <xxlsxx1955@gmail.com>
2023-05-08 21:04:28 +0800

    Add omniauth related class methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
+  class << self
+    def from_omniauth(auth)
+      find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+      end
+    end
+
+    def new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
+    end
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
