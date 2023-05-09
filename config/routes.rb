d8e1a9e05a83881aec86f8a14bbe452a364a5861
Cheong <xxlsxx1955@gmail.com>
2023-05-09 09:05:34 +0800

    Remove users#index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  resources :users, only: %i[index show]
+  resources :users, only: :show
   resources :strangers, only: :index
   resources :friend_requests, only: %i[index create destroy] do
     member do
