c050cda556a1879b83d2f17e1e0ca9f115b4771e
Cheong <xxlsxx1955@gmail.com>
2023-05-10 21:21:02 +0800

    Include user's welcome email
    * Implement #welcome_email inside UserMailer,
      and added respective html and text file.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: "odin-facebook@example.com"
+
+  def welcome_email
+    @user = params[:user]
+    mail to: @user.email, subject: "Thanks for Signing Up"
+  end
 end
